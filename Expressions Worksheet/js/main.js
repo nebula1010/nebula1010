a6cb394d037b611636eb540ab7bdb6491876e4e1
RUSSAY HERNANDEZ <russayhernandez@RUSSAYs-MacBook-Pro.local>
2014-03-05 09:46:28 -0500

    class exercises day 2

---
+++ Expressions Worksheet/js/main.js
@@ -0,0 +1,5 @@
+
+   var radius = 6
+   const Pi = 3.14159265
+   var circ = radius*2*Pi
+   console.log("the circumference of a circle" + cirlcle)
---
+++ day 2 /js/main.js
@@ -0,0 +1,37 @@
+/**
+ * Created by russayhernandez on 3/5/14.
+ */
+
+
+/*
+var test1 = 40;
+var test2 = 60;
+
+ if (test1>test2)
+ {console.log ("beer");
+}else {console.log("falce")}
+*/
+
+
+/*var rnd = Math.random (); // 0<= n <1 between 0 and 1 less than .5 is heads
+console.log(rnd);
+if (rnd<=.5) {
+    console.log("heads")
+}   else{
+    console.log("tails")
+*/
+/*var dice = math.random();
+if (dice<.33){
+    console.log("start");}
+else if (dice>.33 && dice<.66)
+{console.log("middle");
+}else {
+    console.log("end");
+}*/
+var grade = 83;
+if(grade >= 69){
+    console.log ("you pass")
+}
+else (grade >= 80){console.log("you rock")}
+
+
