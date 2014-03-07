d02d21e891a08aa735468cd2fd29ecd1c10a7d44
RUSSAY HERNANDEZ <russayhernandez@RUSSAYs-MacBook-Pro.local>
2014-03-07 15:38:47 -0500

    day 3

---
+++ day 3 notes/js/main.js
@@ -0,0 +1,76 @@
+
+/*var a=2;
+var b=3;
+var c=0;
+
+c=a+b; //c=5 a=2 b=3
+c+=b*2; //=11 a=
+b--;
+c--;
+c*=(a+1) /*
+
+//control structure/ conditional
+//
+var grade1 = 80;
+var grade2 = 90;
+var grade3 = 75;
+var grade4 = 80;
+var grade5 = 26;
+var grade6 = 99;
+var grade7 = 35;
+var grade8 = 100;
+var grade9 = 6;
+var grade10 = 88;
+*/
+
+/*         //1  2  3   4  5  = zero based index number
+var grades =[80,90,80,86,50]; //=array numbers are elements
+
+var firstGrade = grades [0];  //array access notation */
+/*
+var grades = [90,80,100];
+
+var total = grades[0]+grades[1]+grades[2];
+var average = total/3;
+console.log(average);
+*/
+
+/*
+var age = prompt ("how old are you");
+console.log(name + "is FN cool");
+console.log // concatenation // attatching two strings together
+*/
+//var num = prompt('enter a number');
+//console.log(num+300);
+                   //argument
+//var input = prompt('66');
+//var num = parseint(input)
+
+//console.log(num+300);
+
+/*
+//var test "5453";
+//test = parseint(test);
+//var input = parseint(prompt("enter a number")) //parseint converts a string to a number
+
+//var input = prompt("how old are you")
+
+//if (parseint(input)>50){
+  //  console.log("youre old")
+//}
+
+//var grade1 = parseinc(prompt("grade1"))
+//var grade2 = parseinc(prompt("grade2"))
+//var grade3 = parseinc(prompt("grade3"))
+
+//var grades = [grade1,grade2,grade3]
+
+//string number boleans arrays
+*/
+
+var grade1 = parseinc(prompt("grade1"));
+var grade2 = parseinc(prompt("grade2"));
+var grade3 = parseinc(prompt("grade3"));
+
+var grades = [grade1, grade2, grade3]
+
