e74b1db21d08808c488c66c13de2140edf7baa99
RUSSAY HERNANDEZ <russayhernandez@RUSSAYs-MacBook-Pro.local>
2014-03-06 21:42:55 -0500

    excer

--- Expressions Worksheet/js/main.js
+++ Expressions Worksheet/js/main.js
@@ -2,4 +2,4 @@
    var radius = 6
    const Pi = 3.14159265
    var circ = radius*2*Pi
-   console.log("the circumference of a circle" + cirlcle)
+   console.log("the circumference of a circle" + circle)
---
+++ excercise/expressions worksheet/js/rus.js
@@ -0,0 +1,9 @@
+/**
+ * Created by russayhernandez on 3/6/14.
+ */
+var firstNumber = 14;
+secondNumber = 10;
+var answer;
+Answer = 14/10
+alert(answer)
+
