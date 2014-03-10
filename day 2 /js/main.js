ace754333c57a2091c94ceb73ab5dfc0adf5105e
RUSSAY HERNANDEZ <russayhernandez@RUSSAYs-MacBook-Pro.local>
2014-03-10 13:27:27 -0400

    this is a test
    test

--- day 2 /js/main.js
+++ day 2 /js/main.js
@@ -26,9 +26,43 @@ var pi = 3.14
 var circ = radius*2*pi
 console.log("the circumference of a cirlce is"+circ)
 */
-
+/*
 var dogyears = 7
 var humanyears = 1
 var sparkyage =(dogyears"*"humanyears)
 var console.log("sparkyage")
+*/
+/*
+var  number []
+var n1 = math.random();
+var n2 = math.random();
+var n3 = math.random();
+numbers=[n1,n2,n3]
 
+for(var i-1;?;i++){
+    var n = math.random();
+   console.log("loop")
+}
+for(var i=10;i>-1,i--);
+var=0;
+for(var i= 0;i<4;i++;)
+z+=1
+
+var=0;
+far(var i= 0;i<10;i++)
+[console.log(n1,n2,n3)]
+
+var grades = [10,30,80,90,100,95,5,4,3,5]
+
+var grades = [10,30,80,90,100,95]
+var total=0;
+for (var i=0; i<j;i++){
+    total+=grades[i];
+}
+console.log(total/6);
+
+var names = ["bob","lisa","joe","david","carlos"];
+var n = Math.floor(Math.random)()*6+1);
+
+console.log(math.floor(3.6))   //floor drops the decimal
+*/
---
+++ test/js/Main.js
@@ -0,0 +1,4 @@
+/**
+ * Created by russayhernandez on 3/10/14.
+ */
+console.log("Bing");
